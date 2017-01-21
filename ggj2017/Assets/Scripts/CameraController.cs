b7d48081e376e3197084ef49b2b2241c84f7390c
Ryan Andonian <ryanandonian@gmail.com>
2017-01-20 23:31:58 -0800

    incorporates Player and Police models

---
+++ ggj2017/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject G_Target;
+    public Vector3 TargetOffsetVector;
+    public Vector3 CameraGroundVector;
+
+    public Vector3 TargetGroundPosition { get { return G_Target.transform.position + TargetOffsetVector; } }
+    public Vector3 TargetPosition { get { return (G_Target.transform.position + TargetOffsetVector + CameraGroundVector); } }
+    public bool[] debugs = { true, true, true };
+
+    // Use this for initialization
+    private void Start()
+    {
+        // Try to find the player
+        if (G_Target == null)
+        {
+            G_Target = GameObject.Find("Player");
+        }
+        Debug.Assert(G_Target != null);
+    }
+
+    private void Update()
+    {
+        if (debugs[0]) Debug.DrawLine(transform.position, G_Target.transform.position, Color.yellow);
+        if (debugs[2]) Debug.DrawLine(transform.position, TargetPosition, Color.red);
+        Vector3 deltaPos = TargetPosition - transform.position;
+        Debug.DrawRay(transform.position, deltaPos, Color.green);
+        transform.Translate(deltaPos * Time.deltaTime * 3.0f);
+    }
+}
---
+++ ggj2017/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController mCharController;
+
+    private Vector3 mMovementDir;
+    private Vector3 mLookDir;
+
+    // Use this for initialization
+    private void Start()
+    {
+        mMovementDir = new Vector3();
+        mLookDir = new Vector3();
+        mCharController = GetComponent<CharacterController>();
+        Debug.Assert(mCharController != null, "Player needs CharacterController!");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.green);
+        Vector3 mMovementDir = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            mMovementDir.z += 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            mMovementDir.z -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mMovementDir.x += 1.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            mMovementDir.x -= 1.0f;
+        }
+
+        //moveVec.x = Input.GetAxis("Horizontal");
+        mLookDir = Vector3.Lerp(mLookDir, mMovementDir, Time.deltaTime * 2.5f);
+        transform.rotation = Quaternion.LookRotation(mLookDir);
+        mCharController.SimpleMove(mMovementDir * 5.0f);
+    }
+}
