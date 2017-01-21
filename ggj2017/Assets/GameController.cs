39b106b0090ed14b9fcc2e956301d69b2493f109
Ryan Andonian <ryanandonian@gmail.com>
2017-01-21 00:19:15 -0800

    Adds sprinting to player and a sprintbar Adds GameContrller for managing UI (poorly)

---
+++ ggj2017/Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject G_Player;
+    private PlayerController mPlayer;
+    public GameObject G_UI_Root;
+    public GameObject UI_SprintSlider;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Debug.Assert(G_Player != null);
+        Debug.Assert(G_UI_Root != null);
+        Debug.Assert(UI_SprintSlider != null);
+        G_UI_Root.SetActive(true);
+        mPlayer = G_Player.GetComponent<PlayerController>();
+        Debug.Assert(mPlayer != null);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 sprintSliderScale = UI_SprintSlider.transform.localScale;
+        sprintSliderScale.x = Mathf.Max(0f, mPlayer.SprintVal);
+        UI_SprintSlider.transform.localScale = sprintSliderScale;
+    }
+}
--- ggj2017/Assets/Scripts/PlayerController.cs
+++ ggj2017/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,19 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 mMovementDir;
     private Vector3 mLookDir;
+    private const float kTurnSpeed = 0.05f;
+
+    private float mSprint = 1;
+    public const float kMaxSprint = 1;
+    public float SprintVal { get { return mSprint / kMaxSprint; } }
+
+    public const float kSprintRate = 0.01f;
+
+    // Rate at which sprint is recovered
+    public const float kSprintRecovery = 0.003f;
 
     // Use this for initialization
+
     private void Start()
     {
         mMovementDir = new Vector3();
@@ -42,7 +53,26 @@ public class PlayerController : MonoBehaviour
 
         //moveVec.x = Input.GetAxis("Horizontal");
         mLookDir = Vector3.Lerp(mLookDir, mMovementDir, Time.deltaTime * 2.5f);
-        transform.rotation = Quaternion.LookRotation(mLookDir);
-        mCharController.SimpleMove(mMovementDir * 5.0f);
+        mLookDir.x = (mMovementDir.x - mLookDir.x) * kTurnSpeed + mLookDir.x;
+        mLookDir.y = (mMovementDir.y - mLookDir.y) * kTurnSpeed + mLookDir.y;
+        mLookDir.z = (mMovementDir.z - mLookDir.z) * kTurnSpeed + mLookDir.z;
+        if (mMovementDir.x != 0 || mMovementDir.z != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(mLookDir);
+        }
+        float moveSpeed = 10.0f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (SprintVal > 0.0f)
+            {
+                moveSpeed *= 1.5f;
+                mSprint = Mathf.Max(mSprint - kSprintRate, 0f);
+            }
+        }
+        else
+        {
+            mSprint = Mathf.Min(1.0f, mSprint + kSprintRecovery);
+        }
+        mCharController.SimpleMove(mMovementDir * moveSpeed);
     }
 }
