c5ed0bb6845244ba5891ec32d971c7c47f7e5ff2
Ryan Andonian <ryanandonian@gmail.com>
2017-01-22 10:56:13 -0800

    Adds a basic level design

---
+++ ggj2017/Assets/MarkerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerController : MonoBehaviour
+{
+    public GameObject[] LinesTo;
+    // Use this for initialization
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, Vector3.one);
+        foreach (GameObject go in LinesTo)
+        {
+            Debug.DrawLine(transform.position, go.transform.position, Color.magenta);
+        }
+    }
+}
